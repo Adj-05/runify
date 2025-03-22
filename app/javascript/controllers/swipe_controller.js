95c5535231dbf13c30f26d7e45ad1565c4edae0e
Sasha Belpois Baticle <sasha.belpois@gmail.com>
2025-03-22 12:02:07 +0100

    ?

--- app/javascript/controllers/swipe_controller.js
+++ app/javascript/controllers/swipe_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-
 // Connects to data-controller="swipe"
 export default class extends Controller {
   static targets = ["input1", "averageSpeed", "trainingDuration", "musicGenre", "button1", "button2", "progressBar", "form"];
-
   connect() {
     this.screens = [this.averageSpeedTarget, this.trainingDurationTarget, this.musicGenreTarget];
     this.currentScreenIndex = 0;
@@ -11,7 +9,6 @@ export default class extends Controller {
     this.updateScreens();
     console.log("Stimulus controller connected");
   }
-
   // swipe1() {
   //   setTimeout(() => {
   //     console.log("Retardée d'une seconde.");
@@ -19,9 +16,7 @@ export default class extends Controller {
   //     this.trainingDurationTarget.classList.remove("d-none");
   //     this.button1Target.classList.remove("d-none");
   //   }, "1000");
-
   // }
-
   // swipe2() {
   //   setTimeout(() => {
   //     console.log("Retardée d'une seconde.");
@@ -30,76 +25,60 @@ export default class extends Controller {
   //     this.button2Target.classList.remove("d-none");
   //     this.musicGenreTarget.classList.remove("d-none");
   //   }, "1000");
-
   // }
-
   // back1() {
   //   this.averageSpeedTarget.classList.remove("d-none");
   //   this.trainingDurationTarget.classList.add("d-none");
   //   this.button1Target.classList.add("d-none");
   // }
-
   // back2() {
   //   this.trainingDurationTarget.classList.remove("d-none");
   //   this.musicGenreTarget.classList.add("d-none");
   //   this.button2Target.classList.add("d-none");
   //   this.button1Target.classList.remove("d-none");
   // }
-
   // sendForm(event) {
   //   event.preventDefault();
   //   this.updateSelection(event);
   //   setTimeout(() => this.redirectToPreview(), 200);
   // }
-
   swipe1(event) {
     this.updateSelection(event);
     setTimeout(() => this.nextScreen(), 200);
   }
-
   swipe2(event) {
     this.updateSelection(event);
     setTimeout(() => this.nextScreen(), 200);
   }
-
-
   back1() {
     this.previousScreen();
   }
-
   back2() {
     this.previousScreen();
   }
-
   nextScreen() {
     if (this.currentScreenIndex < this.screens.length - 1) {
       this.currentScreenIndex++;
       this.updateScreens();
     }
   }
-
   previousScreen() {
     if (this.currentScreenIndex > 0) {
       this.currentScreenIndex--;
       this.updateScreens();
     }
   }
-
   updateScreens() {
     this.screens.forEach((screen, index) => {
       screen.style.display = index === this.currentScreenIndex ? "flex" : "none";
     });
-
     this.progressBarTarget.innerText = `${this.currentScreenIndex + 1} / ${this.screens.length}`;
-
     this.button1Target.style.display = this.currentScreenIndex > 0 ? "block" : "none";
     this.button2Target.style.display = this.currentScreenIndex > 1 ? "block" : "none";
   }
-
   updateSelection(event) {
     const selectedInput = event.target;
     const allInputs = selectedInput.closest(".options").querySelectorAll("input");
-
     allInputs.forEach(input => {
       const label = input.nextElementSibling;
       if (label) {
@@ -107,17 +86,14 @@ export default class extends Controller {
         label.style.color = "white";
       }
     });
-
     const selectedLabel = selectedInput.nextElementSibling;
     if (selectedLabel) {
-      selectedLabel.style.background = "#64dd17";
+      selectedLabel.style.background = "#64DD17";
       selectedLabel.style.color = "white";
     }
   }
-
   sendForm(event) {
     event.preventDefault();
-
     fetch(this.formTarget.action, {
       method: "POST",
       headers: { "Accept": "application/json" },
@@ -134,3 +110,8 @@ export default class extends Controller {
       });
   }
 }
+
+
+
+
+NOUVEAU
